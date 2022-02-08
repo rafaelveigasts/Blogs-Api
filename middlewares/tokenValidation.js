749d16edf27187b3bea48910970b88c1c84f9dc4
rafael veiga <rafaelveig@hotmail.com>
2022-02-07 22:36:42 -0300

    cria validacao de token

---
+++ middlewares/tokenValidation.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const User = require('../models/user');
+
+const secret = process.env.JWT_SECRET;
+
+module.exports = async (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.status(401).json({ message: 'Token not found' });
+  }
+
+  try {
+    const decode = jwt.verify(authorization, secret);
+    const user = await User.findOne({
+      where: { username: decode.data.email },
+    });
+
+    if (!user) {
+      return res.status(409).json({ message: 'User not found' });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Expired or invalid token' });
+  }
+};
