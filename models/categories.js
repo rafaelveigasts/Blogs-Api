4329f55d4d9d2d485bc8fd33ae1bcf3d2ffe1a52
rafael veiga <rafaelveig@hotmail.com>
2022-02-08 15:31:09 -0300

    refatora modelo de categorias

--- models/categories.js
+++ models/categories.js
@@ -1,13 +1,11 @@
 module.exports = (sequelize, DataTypes) => {
-  const Categories = sequelize.define('Categories', {
-    name: {
-      type: DataTypes.STRING,
-    },
+  const Categorie = sequelize.define('Categories', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: { type: DataTypes.STRING },
   },
-  { 
+  {
     timestamps: false,
     tableName: 'Categories',
   });
-
-  return Categories;
+  return Categorie;
 };
