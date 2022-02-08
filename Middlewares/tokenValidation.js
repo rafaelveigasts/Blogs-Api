d117f0f6bad09d5afbf49d78f49f5be5b8cacee8
rafael veiga <rafaelveig@hotmail.com>
2022-02-08 19:08:23 -0300

    teste

--- Middlewares/tokenValidation.js
+++ Middlewares/tokenValidation.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const { User } = require('../models/user');
+const { User } = require('../models/User');
 
 const secret = process.env.JWT_SECRET;
 
