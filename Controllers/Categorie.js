5efb82b389013835985b7519d932055bcfd689a2
rafael veiga <rafaelveig@hotmail.com>
2022-02-14 16:36:33 -0300

    getall cat no controller

--- Controllers/Categorie.js
+++ Controllers/Categorie.js
@@ -10,10 +10,23 @@ const createCategorie = async (req, res) => {
   }
 };
 
+const getAllCategories = async (req, res) => {
+  try {
+    const categories = await ServiceCategorie.getAllCategories();
+    return res.status(200).json(categories);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
 module.exports = {
   createCategorie,
+  getAllCategories,
 };
 
 /*  aqui retiramos o name do body e aplicamos a criação de categoria passando o name fornecido
 caso tenha sucesso volta o 201 com a categoria
-se não tiver volta erro */
+se não tiver volta erro 
+
+para as categorias, usamos o findAll que retorna todas as categorias cadastradas no banco de dados
+*/
