7210ca32fed52deacc26e0ce72685b01fd904655
rafael veiga <rafaelveig@hotmail.com>
2022-02-14 23:31:56 -0300

    getallposts service

--- Services/BlogPost.js
+++ Services/BlogPost.js
@@ -1,4 +1,4 @@
-const { BlogPost, PostsCategorie, Categorie } = require('../models');
+const { BlogPost, PostsCategorie, Categorie, User } = require('../models');
 
 const categorieExists = (array) => {
   const result = array.reduce(async (ac, cur) => {
@@ -45,6 +45,16 @@ const createPost = async (objPost, userId) => {
   };
 };
 
+const getAllPosts = async () => {
+  const posts = await BlogPost.findAll({
+    include: [{
+      model: User, as: 'user'},
+      { model: Categorie, as: 'categories'}],
+  });
+  return posts;
+};
+
 module.exports = {
   createPost,
+  getAllPosts,
 };
