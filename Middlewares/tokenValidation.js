a5debd3aa845a658d04929b4f9327212d7f096cd
rafael veiga <rafaelveig@hotmail.com>
2022-02-14 22:08:41 -0300

    refatora validacao do token

--- Middlewares/tokenValidation.js
+++ Middlewares/tokenValidation.js
@@ -1,9 +1,10 @@
 const jwt = require('jsonwebtoken');
+const ServiceUser = require('../Services/User');
 require('dotenv').config();
 
 const secret = process.env.JWT_SECRET;
 
-const checkToken = (req, res, next) => {
+const checkToken = async (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization) {
@@ -11,7 +12,12 @@ const checkToken = (req, res, next) => {
   }
 
   try {
-    jwt.verify(authorization, secret);
+    const decode = jwt.verify(authorization, secret);
+    const user = await ServiceUser.getEmail(decode.user);
+    if (!user) {
+      return res.status(401).json({ message: 'Erro ao procurar usuário do token.' });
+    }
+    req.user = user;
     next();
   } catch (error) {
     return res.status(401).json({ message: 'Expired or invalid token' });
